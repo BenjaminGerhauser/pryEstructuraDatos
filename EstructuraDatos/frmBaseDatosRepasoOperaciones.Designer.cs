a41c486c3afa60947d83626a93c9503f2d36c823
benja <benja@DESKTOP-2SSPID2>
2024-05-26 22:43:38 -0300

    20 casos terminados, falta completar la descripcion

--- EstructuraDatos/frmBaseDatosRepasoOperaciones.Designer.cs
+++ EstructuraDatos/frmBaseDatosRepasoOperaciones.Designer.cs
@@ -28,20 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboSQL = new System.Windows.Forms.ComboBox();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.Grilla = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.Grilla)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Operacion a realizar en la base de datos:";
+            // 
+            // cboSQL
+            // 
+            this.cboSQL.FormattingEnabled = true;
+            this.cboSQL.Items.AddRange(new object[] {
+            "ProyeccionSimple",
+            "ProyeccionSimple2",
+            "ProyeccionMultiatributo",
+            "ProyeccionMultiatributo2",
+            "Juntar",
+            "Juntar2",
+            "SeleccionSimple",
+            "SeleccionSimple2",
+            "SeleccionMultiatributo",
+            "SeleccionMultiatributo2",
+            "SeleccionConvolucion",
+            "SeleccionConvolucion2",
+            "Union",
+            "Union2",
+            "Interseccion",
+            "Interseccion2",
+            "Diferencia",
+            "Diferencia2",
+            "ProyeccionSimple3",
+            "ProyeccionSimple4"});
+            this.cboSQL.Location = new System.Drawing.Point(221, 13);
+            this.cboSQL.Name = "cboSQL";
+            this.cboSQL.Size = new System.Drawing.Size(370, 21);
+            this.cboSQL.TabIndex = 1;
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(597, 11);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(87, 23);
+            this.btnListar.TabIndex = 2;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblDescripcion.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.Location = new System.Drawing.Point(15, 60);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(669, 122);
+            this.lblDescripcion.TabIndex = 3;
+            // 
+            // Grilla
+            // 
+            this.Grilla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grilla.Location = new System.Drawing.Point(15, 214);
+            this.Grilla.Name = "Grilla";
+            this.Grilla.Size = new System.Drawing.Size(669, 236);
+            this.Grilla.TabIndex = 4;
             // 
             // frmBaseDatosRepasoOperaciones
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(699, 462);
+            this.Controls.Add(this.Grilla);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.cboSQL);
+            this.Controls.Add(this.label1);
             this.Name = "frmBaseDatosRepasoOperaciones";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmBaseDatosRepasoOperaciones";
+            ((System.ComponentModel.ISupportInitialize)(this.Grilla)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboSQL;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.DataGridView Grilla;
     }
 }
--- EstructuraDatos/frmBaseDatosRepasoOperaciones.cs
+++ EstructuraDatos/frmBaseDatosRepasoOperaciones.cs
@@ -16,5 +16,134 @@ namespace EstructuraDatos
         {
             InitializeComponent();
         }
+        clsBaseDatos db = new clsBaseDatos();
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            string sql = "";
+            switch (cboSQL.SelectedIndex)
+            {
+                case 0:
+                    sql = "SELECT Titulo FROM Libro";
+                    db.Listar(Grilla,sql);
+                    break;
+                case 1:
+                    sql = "SELECT Nombre FROM Idioma";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 2:
+                    sql = "SELECT Titulo,Año FROM Libro WHERE Precio < 800";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 3:
+                    sql = "SELECT IdAutor,Nombre FROM Autor WHERE IdAutor < 10";
+                    db.Listar(Grilla,sql);
+
+                    break;
+                case 4:
+                    sql = @"SELECT Libro.Titulo, Autor.Nombre
+                        FROM Libro
+                        INNER JOIN Autor ON Libro.IdAutor = Autor.IdAutor";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 5:
+                    sql = @"SELECT Libro.Titulo, Idioma.Nombre
+                        FROM Libro
+                        INNER JOIN Idioma ON Libro.IdIdioma = Idioma.IdIdioma";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 6:
+                    sql = "SELECT * FROM Libro WHERE IdIdioma = 2";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 7:
+                    sql = "SELECT * FROM Pais";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 8:
+                    sql = "SELECT Titulo,Año,Precio FROM Libro WHERE Precio = 125";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 9:
+                    sql = "SELECT Titulo,Año,Precio FROM Libro WHERE Precio> 800";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 10:
+                    sql = @"SELECT * FROM 
+                    (SELECT * FROM Libro as T1 WHERE T1.IdIdioma > 5) as T2 WHERE T2.IdAutor > 10";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 11:
+                    sql = @"SELECT * FROM 
+                    (SELECT * FROM Libro as T1 WHERE T1.Precio > 300) as T2 WHERE T2.IdIdioma > 3";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 12:
+                    sql = @"SELECT * FROM Libro WHERE IdIdioma = 3
+                        UNION
+                        SELECT * FROM Libro WHERE IdIdioma = 4
+                        UNION
+                        SELECT * FROM Libro WHERE IdIdioma = 2";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 13:
+                    sql = @"SELECT * FROM Libro WHERE Precio = 125
+                        UNION
+                        SELECT * FROM Libro WHERE Precio = 854
+                        UNION
+                        SELECT * FROM Libro WHERE Precio = 789";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 14:
+                    sql = @"SELECT * FROM Libro WHERE IdIdioma
+                            IN 
+                            (SELECT DISTINCT IdIdioma FROM Libro WHERE IdIdioma < 5)";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 15:
+                    sql = @"SELECT * FROM Libro WHERE IdAutor
+                            IN 
+                            (SELECT DISTINCT IdAutor FROM Libro WHERE IdAutor > 70)";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 16:
+                    sql = @"SELECT * FROM Libro WHERE IdIdioma
+                    NOT IN
+                    (SELECT DISTINCT IdIdioma FROM Libro WHERE IdIdioma < 5)";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 17:
+                    sql = @"SELECT * FROM Libro WHERE IdAutor
+                    NOT IN
+                    (SELECT DISTINCT IdAutor FROM Libro WHERE IdAutor > 50)";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 18:
+                    sql = "SELECT Nombre FROM Idioma";
+                    db.Listar(Grilla, sql);
+
+                    break;
+                case 19:
+                    sql = "SELECT Nombre FROM Autor";
+                    db.Listar(Grilla, sql);
+
+                    break;
+            }
+        }
     }
 }
